32b3066787eeb0ca5128ea08854a8d2d2b2f1696
SupermanWY <1055190598@qq.com>
2018-12-15 13:29:06 +0800

    实现音乐播放

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 $(function() {
+  var isPlaying = false
   var html = ''
   for (var i = 1; i <= 30; i++) {
     html += '<div class="swiper-slide"><img src="./img/s'+ i +'.jpg" /></div>'
@@ -14,7 +15,51 @@ $(function() {
     },
   })
 
+  var audio = $('.audio')[0]
+  var startAudio = startAudioProxy()
+
   $('.next-btn').on('click', function() {
     swiper.slideNext()
   })
+  $('.audio-play-btn').on('click', function() {
+    isPlaying ? endAudio() : startAudio()
+  })
+
+  audio.addEventListener('canplay', startAudio)
+
+  function startAudioProxy() {
+    var hasPlayed = false
+
+    return function() {
+      audio.play().then(function() {
+        onAudioStart()
+      }).catch(function () {
+        var arr = ['touchend', 'click', 'doubleclick', 'keydown']
+        arr.forEach(function(item) {
+          document.addEventListener(item, function(){
+            if (hasPlayed) {
+              document.removeEventListener(item, arguments.callee)
+            }
+            audio.play()
+            onAudioStart()
+            hasPlayed = true
+          })
+        })
+      })
+    }
+  }
+
+  function endAudio() {
+    audio.pause()
+    onAudioEnd()
+  }
+
+  function onAudioStart() {
+    isPlaying = true
+    $('.audio-play-btn').css('animation', 'rotate 2s infinite linear')
+  }
+  function onAudioEnd() {
+    isPlaying = false
+    $('.audio-play-btn').css('animation', '')
+  }
 })
